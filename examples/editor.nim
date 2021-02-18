c913d4657d4f5437415612eb2db9306d32a9f8c2
Productive2 <48047721+Productive2@users.noreply.github.com>
2021-02-18 22:27:36 +0100

    now editor wont wipe files when ctrl+c

--- examples/editor.nim
+++ examples/editor.nim
@@ -14,11 +14,15 @@ if paramCount() > 0:
   let arg = paramStr(1)
   if fileExists(arg):
     e.content = readFile(arg).fromString()
+
+var save = false
 e.bindKey("ctrl+s"):
   e.finish()
+  save = true
+
 e.prompt = ""
 e.populateDefaults(enterSubmits = false, shiftForVerticalMove = false)
 let result = e.read()
-if paramCount() > 0:
+if save and paramCount() > 0:
   writeFile(paramStr(1), result)
 
